743b891d51f6ae6881713a124cd18e310b091c02
yueranl <yueranl03@gmail.com>
2025-04-06 18:19:58 +0200

    Using event trigger to implement the state machine

--- src/client/src/client.cc
+++ src/client/src/client.cc
@@ -9,15 +9,40 @@
 #include<arpa/inet.h>
 #include<netdb.h>
 #include<vector>
+#include<thread>
+#include<future>
 
 #include "MessageType.hh"
 #include "ConnectMessage.hh"
 #include "utility/MessageConstructor.hh"
 #include "client.hh"
 
+enum class StateClient // should be integrated to clinet class later
+{
+    Init,
+    HandShaked
+};
+
+void read_event(std::future<std::vector<uint8_t>>& msg, StateClient& state)
+{
+    if(msg.get()[0] == static_cast<uint8_t>(MessageType::Accept))
+    {
+        state = StateClient::HandShaked;
+        std::cout << "Handshake with Server accomplished!" << std::endl;
+    }
+}
+
 int main()
 {
+
     tcp_client c;
+    std::promise<std::vector<uint8_t>> prom_msg;
+    std::future<std::vector<uint8_t>> fut_msg = prom_msg.get_future();
+
+    // start state machine of client
+    StateClient state = StateClient::Init;
+
+    std::thread th1(read_event, std::ref(fut_msg), std::ref(state));
     // Step 1: send Connect Message
     ConnectMessage messages = {
         .type = MessageType::Connect,
@@ -34,9 +59,10 @@ int main()
      
     //receive and echo reply
     cout<<"----------------------------\n\n";
-    cout << unsigned(c.receive(1024)[0]);
+    prom_msg.set_value(c.receive(1024));
     cout<<"\n\n----------------------------\n\n";
-     
+
+    th1.join();
     //done
     return 0;
 }
