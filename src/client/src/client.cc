9298b650608bba225bea4574dcfd6685c9d80345
yueranl <yueranl03@gmail.com>
2025-04-07 20:49:57 +0200

    Update state machine and transition

--- src/client/src/client.cc
+++ src/client/src/client.cc
@@ -25,40 +25,48 @@ enum class StateClient // should be integrated to clinet class later
     HandShaked
 };
 
-// void read_event(std::future<std::vector<uint8_t>>& msg, StateClient& state)
-// {
-//     if(msg.get()[0] == static_cast<uint8_t>(MessageType::Accept) && state == StateClient::Idle)
-//     {
-//         state = StateClient::HandShaked;
-//         std::cout << "Handshake with Server accomplished!" << std::endl;
-//         // send StoreMessage
-//     }
-
-//     while(1)
-//     {
-        
-//     }
-// }
 std::future<std::vector<uint8_t>> async_recv() {
     return std::async(std::launch::async, [] {
         return c.receive(1024);
     });
 }
 
+std::future<bool> async_send(const std::vector<uint8_t>& msg) {
+    return std::async(std::launch::async, [&msg] {
+        return c.send_data(msg);
+    });
+}
+
 void on_idle(StateClient& state)
 {
     std::cout << "Client is idle" << std::endl;
-    std::future<std::vector<uint8_t>> fut_msg = async_recv();
-    std::vector<uint8_t> msg = fut_msg.get();
-    if(msg[0] == static_cast<uint8_t>(MessageType::Accept) && state == StateClient::Idle)
-    {
-        state = StateClient::HandShaked;
-        std::cout << "Handshake with Server accomplished!" << std::endl;
-        // send StoreMessage
-    }
-    else
-    {
-        std::cout << "Received message: " << msg[0] << std::endl;
+
+    // Step 1: send Connect Message
+    ConnectMessage messages = {
+        .type = MessageType::Connect,
+        .size_pyld = 1
+    };
+
+    std::vector<uint8_t> con_msg = construct_connectMsg(messages);
+
+    auto send_future = async_send(con_msg);
+    if (send_future.get()) {
+        std::cout << "Connect message sent!" << std::endl;
+        std::future<std::vector<uint8_t>> fut_msg = async_recv();
+        std::vector<uint8_t> msg = fut_msg.get();
+        if(msg[0] == static_cast<uint8_t>(MessageType::Accept) && state == StateClient::Idle)
+        {
+            state = StateClient::HandShaked;
+            std::cout << "Handshake with Server accomplished!" << std::endl;
+            // send StoreMessage
+        }
+        else
+        {
+            std::cout << "Received message: " << msg[0] << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to send connect message!" << std::endl;
+        return;
     }
 }
 
@@ -89,19 +97,10 @@ int main()
     // start state machine of client
     StateClient state = StateClient::Idle;
 
-    // Step 1: send Connect Message
-    ConnectMessage messages = {
-        .type = MessageType::Connect,
-        .size_pyld = 1
-    };
-
-    std::vector<uint8_t> con_msg = construct_connectMsg(messages);
 
     //connect to host
     c.conn("localhost" , 8080);
-     
-    //send some data
-    c.send_data(con_msg);
+    std::cout<<"Connected to server\n";
      
     //receive and echo reply
     cout<<"----------------------------\n\n";
